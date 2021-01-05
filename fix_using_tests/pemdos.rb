1da914401103db89889e962f08c4a56d01099287
calm-collection-3916 <khalinredd@gmail.com>
2021-01-05 01:21:18 +0000

    Done.

--- fix_using_tests/pemdos.rb
+++ fix_using_tests/pemdos.rb
@@ -1,9 +1,10 @@
-# don't forget to add: require 'pry'
+
 
 def snake_it_up(string)
-  if string[0] == "s"
-    10 * "s" + string
-  else
-    string
+  
+  name = "#{string}" 
+  if string[0] == "s" 
+    name.insert(0, "ssssssssss")
   end
 end
+snake_it_up("surprise!")
